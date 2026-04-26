23a2d45267011d00f90c061879f50ac0ab160b04
thislookshard <luedtkesj@gmail.com>
2026-04-26 13:00:56 -0500

    Changing where processMonitor is instantiated

--- WindowsGameLauncher/Program.cs
+++ WindowsGameLauncher/Program.cs
@@ -11,13 +11,13 @@ internal class Program
 
         var logger = new LogService(logDirectoryPath);
         var profileService = new ProfileService();
-
         logger.Info("Windows Game Launcher started.");
         try
         {
             var profile = profileService.Load(profilePath);
             var launcher = new LauncherService(logger);
-            var watchdog = new WatchdogService(logger, launcher, new ProcessMonitorService(logger));
+            var processMonitor = new ProcessMonitorService(logger);
+            var watchdog = new WatchdogService(logger, launcher, processMonitor);
             await watchdog.RunWithRecoveryAsync(profile);
             logger.Info("Launcher run completed.");
             return 0;
